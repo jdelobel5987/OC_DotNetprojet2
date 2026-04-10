d5944629be4c74be5c55e6d036731b7859824fb3
jdelobel5987 <delobel.julien@outlook.com>
2026-04-10 17:10:47 +0200

    feat(product): update product stock after order
    - implement UpdateProductQuantities in ProductService
    - make the ProductRepository instance as Singleton

--- P2FixAnAppDotNetCode/Models/Services/ProductService.cs
+++ P2FixAnAppDotNetCode/Models/Services/ProductService.cs
@@ -40,8 +40,10 @@ namespace P2FixAnAppDotNetCode.Models.Services
         /// </summary>
         public void UpdateProductQuantities(Cart cart)
         {
-            // TODO implement the method
-            // update product inventory by using _productRepository.UpdateProductStocks() method.
+            foreach (CartLine line in cart.Lines)
+            {
+                _productRepository.UpdateProductStocks(line.Product.Id, line.Quantity);
+            }
         }
     }
 }
--- P2FixAnAppDotNetCode/Startup.cs
+++ P2FixAnAppDotNetCode/Startup.cs
@@ -29,7 +29,7 @@ namespace P2FixAnAppDotNetCode
             services.AddSingleton<ICart, Cart>();
             services.AddSingleton<ILanguageService, LanguageService>();
             services.AddTransient<IProductService, ProductService>();
-            services.AddTransient<IProductRepository, ProductRepository>();
+            services.AddSingleton<IProductRepository, ProductRepository>();
             services.AddTransient<IOrderService, OrderService>();
             services.AddTransient<IOrderRepository, OrderRepository>();
             services.AddMemoryCache();
